1c46f6d3b56c0bb83bb1a7b52afae290f85ce28d
Joan-Buck <buck.joan@gmail.com>
2022-03-15 14:11:09 -0600

    updating user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
+from datetime import datetime
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -9,8 +9,16 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
 
+    trucks = db.Relationship("Truck", back_populates="users")
+    reviews = db.Relationship("Review", back_populates="users")
+    truck_images = db.Relationship("TruckImage", back_populates="users")
+    
     @property
     def password(self):
         return self.hashed_password
@@ -26,5 +34,7 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'firstName': self.first_name,
+            'lastName': self.last_name
         }
