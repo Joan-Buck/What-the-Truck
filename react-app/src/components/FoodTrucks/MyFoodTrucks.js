0271c9ce70c669e12c0aaf3adf5047c348ddd9ba
Joan-Buck <buck.joan@gmail.com>
2022-03-16 15:16:44 -0600

    Rendering new food truck form

--- react-app/src/components/FoodTrucks/MyFoodTrucks.js
+++ react-app/src/components/FoodTrucks/MyFoodTrucks.js
@@ -1,24 +1,33 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMyFoodTrucksThunk } from '../../store/foodTrucks';
 import MyFoodTruckCard from './MyFoodTruckCard';
+import NewFoodTruckForm from './NewFoodTruckForm';
 
 const MyFoodTruckListing = () => {
     const dispatch = useDispatch();
     const myFoodTrucksObj = useSelector(state => state.foodTrucks.foodTrucks);
     const foodTrucks = Object.values(myFoodTrucksObj);
-
+    const [renderForm, setRenderForm] = useState(false);
 
     useEffect(() => {
         dispatch(getMyFoodTrucksThunk())
     }, [dispatch])
 
+    const showForm = (e) => {
+        e.preventDefault();
+        setRenderForm(true)
+    }
+
     return (
         <div>
-            <button>
+            <button onClick={showForm}>
                 Add New Food Truck
             </button>
-             {foodTrucks.map((foodTruck) => (
+            {renderForm && (
+                <NewFoodTruckForm />
+            )}
+            {foodTrucks.map((foodTruck) => (
                // TO DO: link entire card to truck detail page
                 <MyFoodTruckCard key={foodTruck.id} foodTruck={foodTruck}/>
             ))}
