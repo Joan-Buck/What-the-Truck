12f20acbc99f186377573f6fc49c43a4588de57c
Joan-Buck <buck.joan@gmail.com>
2022-03-18 11:19:40 -0600

    Adding demo button and sign up button to login form

--- react-app/src/components/auth/LoginFormModal/LoginForm.js
+++ react-app/src/components/auth/LoginFormModal/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 import { login } from '../../../store/session';
+import * as sessionActions from '../../../store/session';
 import './LoginForm.css';
 
 
@@ -32,8 +33,20 @@ const LoginForm = () => {
     return <Redirect to='/' />;
   }
 
+  const demoLogin = async (e) => {
+    e.preventDefault();
+    const email = 'demo@aa.io';
+    const password = 'password';
+
+    dispatch(sessionActions.login(email, password));
+    // history.push('/');
+    // TO DO: add redirect to whatever page makes sense on login
+    return <Redirect to="/" />
+  }
+
   return (
     <div className='login-form-component'>
+      <div className='login-form-component-container'>
       <form onSubmit={onLogin} className='login-form-component-form-container'>
         <ul className='form-errors'>
           {errors.map((error, i) => (
@@ -41,29 +54,40 @@ const LoginForm = () => {
           ))}
         </ul>
         {/* <div> */}
-          <label htmlFor='email' className='form-label'>Email</label>
-          <input
-            name='email'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={updateEmail}
-            className='form-input'
-          />
+        <label htmlFor='email' className='form-label'>Email</label>
+        <input
+          name='email'
+          type='text'
+          placeholder='Email'
+          value={email}
+          onChange={updateEmail}
+          className='form-input'
+        />
         {/* </div> */}
         {/* <div> */}
-          <label htmlFor='password' className='form-label'>Password</label>
-          <input
-            name='password'
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={updatePassword}
-            className='form-input'
-          />
-          <button type='submit' className='form-submit-btn'>Login</button>
+        <label htmlFor='password' className='form-label'>Password</label>
+        <input
+          name='password'
+          type='password'
+          placeholder='Password'
+          value={password}
+          onChange={updatePassword}
+          className='form-input'
+        />
+        <button type='submit' className='form-submit-btn'>Login</button>
         {/* </div> */}
       </form>
+      <div className='login-form-component-demo-container'>
+        <button className='login-form-component-demo-btn' onClick={demoLogin}>
+          Login as Demo User
+        </button>
+      </div>
+      <div className='login-form-component-signup-container'>
+        <NavLink to='/sign-up' exact={true} className='login-form-component-signup-btn'>
+          Sign up for an account...
+        </NavLink>
+      </div>
+      </div>
     </div>
   );
 };
