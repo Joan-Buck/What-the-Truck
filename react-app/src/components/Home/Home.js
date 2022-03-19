207e0e74f25d5d709dd6700f6ababdc53f26d9ce
Joan-Buck <buck.joan@gmail.com>
2022-03-19 15:10:52 -0600

    Overlaying text on image on home page

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -1,16 +1,14 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { getFoodTrucksThunk } from '../../store/foodTrucks';
 import './Home.css';
 
 function Home() {
     const dispatch = useDispatch();
-    const foodTrucksObj = useSelector(state => state.foodTrucks.entities);
-    const foodTrucks = Object.values(foodTrucksObj)
-    // TO DO: render random food trucks from DB
-    // const foodTruck1 = foodTrucks[0]
-    // const foodTruck2 = foodTrucks[1]
-    // const foodTruck3 = foodTrucks[2]
+    // const foodTrucksObj = useSelector(state => state.foodTrucks.entities);
+    // const foodTrucks = Object.values(foodTrucksObj)
+
 
     useEffect(() => {
         dispatch(getFoodTrucksThunk())
@@ -18,14 +16,17 @@ function Home() {
 
     return (
         <div className='home-component-container'>
-            <div className='home-component-banner-div'>
-                <div className='home-component-banner-img-container'>
-                    <img className='home-component-banner-img' src='https://www.forafinancial.com/wp-content/uploads/2020/01/foodtruck.jpg' alt='Food Truck'/>
+            <NavLink to='/food-trucks' exact={true} className='home-component-banner-div-link'>
+                <div className='home-component-banner-div'>
+                    <div className='home-component-banner-img-container'>
+                        <img className='home-component-banner-img' src='https://www.forafinancial.com/wp-content/uploads/2020/01/foodtruck.jpg' alt='Food Truck' />
+                    </div>
+                    <h1 className='home-component-banner-text'>
+                        Find where to eat...
+                    </h1>
                 </div>
-                <h1 className='home-component-banner-text'>
-                    Find where to eat...
-                </h1>
-            </div>
+
+            </NavLink>
         </div>
     )
 }
