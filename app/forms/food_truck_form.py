59bb6d0073a3e7db6e31efb0ec8987a9a3afa18f
Joan-Buck <buck.joan@gmail.com>
2022-03-19 11:31:37 -0600

    Validating that zip code is 5 digits

--- app/forms/food_truck_form.py
+++ app/forms/food_truck_form.py
@@ -1,14 +1,23 @@
 from random import choices
 from flask import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, IntegerField
-from wtforms.validators import DataRequired, InputRequired, Length
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired, InputRequired, Length, ValidationError
+
+def validate_zip_code(form, field):
+        zip_code = field.data
+
+        if zip_code.isnumeric() is False:
+            raise ValidationError('Zip code must be 5 digits.')
 
 class FoodTruckForm(FlaskForm):
-    name = StringField("name", validators=[InputRequired(message="Please provide a name for your food truck."), Length(min=1, max=100, message="Please limit your food truck's name to 100 characters or less.")])
-    address = StringField("address", validators=[InputRequired(message="Please provide an address for your food truck."), Length(min=1, max=100, message="Please limit your food truck's address to 100 characters or less.")])
-    city = StringField("city", validators=[InputRequired(message="Please provide the city for your food truck."), Length(min=1, max=100, message="Please limit your food truck's city to 100 characters or less.")])
-    state = SelectField("state", validators=[InputRequired(message="Please select 1 US state for your food truck.. Our app is currently only serving US based food trucks.")],
+    name = StringField("name", validators=[InputRequired(message="Please provide a name for your food truck."),
+                Length(min=1, max=100, message="Please limit your food truck's name to 100 characters or less.")])
+    address = StringField("address", validators=[InputRequired(message="Please provide an address for your food truck."),
+                Length(min=1, max=100, message="Please limit your food truck's address to 100 characters or less.")])
+    city = StringField("city", validators=[InputRequired(message="Please provide the city for your food truck."),
+                Length(min=1, max=100, message="Please limit your food truck's city to 100 characters or less.")])
+    state = SelectField("state", validators=[InputRequired(message="Please select 1 US-based state for your food truck. Our app is currently only serving US based food trucks.")],
                 choices=['Alabama', 'Alaska', 'Arizona', 'Arkansas',
                 'California', 'Colorado', 'Connecticut',
                 'Delaware',
@@ -28,7 +37,9 @@ class FoodTruckForm(FlaskForm):
                 'Utah',
                 'Vermont', 'Virginia',
                 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'])
-    zip_code = StringField("zip_code", validators=[InputRequired(message="Please provide the US zip code for your food truck. Our app is currently only serving US based food trucks."), Length(min=1, max=5, message="Please enter a 5 digit US zip code. Our app is currently only serving US based food trucks.")])
+    zip_code = StringField("zip_code", validators=[InputRequired(message="Please provide the 5 digit US zip code for your food truck. Our app is currently only serving US based food trucks."),
+                validate_zip_code,
+                Length(min=5, max=5, message="Please enter a 5 digit US zip code. Our app is currently only serving US based food trucks.")])
     cuisine = SelectField("cuisine", validators=[InputRequired(message="Please select 1 cuisine type for your food truck. Our app is currently only serving food trucks with these cuisines.")],
             choices=['Tacos/Burritos', 'Sandwiches', 'Coffee', 'BBQ', 'Ice Cream', 'Dessert', 'Sushi', 'Indian'])
     price = SelectField("price", validators=[InputRequired(message="Please select 1 price category for your food truck.")],
