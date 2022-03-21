538d5e8053cdf2c60cf30a77b38f5f87b6aabb6e
Joan-Buck <buck.joan@gmail.com>
2022-03-21 10:12:50 -0600

    Adding min length validations on email, username, email, pass, and confirm pass in sign up form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -25,23 +25,24 @@ class SignUpForm(FlaskForm):
     username = StringField('username',
         validators=[DataRequired(message="Please enter a username."),
         username_exists,
-        Length(min=1, max=40, message="Please enter a username less than 40 characters long.")])
+        Length(min=2, max=40, message="Please enter a username between 2 and 40 characters long.")])
     email = StringField('email',
         validators=[DataRequired(message="Please enter a valid email address."),
         user_exists,
-        Length(min=1, max=255, message='Please enter an email less than 255 characters long.'),
+        Length(min=4, max=255, message='Please enter an email between 4 and 255 characters long.'),
         Email(message='Please enter a valid email address.')])
         # TO DO: email can be a weird email address ( ex: test@email.comasdlkfjasdl;fkj)
     first_name = StringField('first_name',
         validators=[DataRequired(message="Please enter your first name."),
-        Length(min=1, max=50, message="Please enter a first name less than 50 characters.")])
+        Length(min=2, max=50, message="Please enter a first name between 2 and 50 characters long.")])
     last_name = StringField('last_name',
         validators=[DataRequired(message='Please enter your last name.'),
-        Length(min=1, max=50, message="Please enter a last name less than 50 characters.")])
+        Length(min=2, max=50, message="Please enter a last name between 2 and 50 characters long.")])
     password = StringField('password',
         validators=[InputRequired(message="Please enter a password."),
-        Length(min=1, max=255, message='Please enter a password less than 255 characters long.'),
+        Length(min=4, max=255, message='Please enter a password between 4 and 255 characters long.'),
         EqualTo('confirm_password', message='Passwords must match.')])
     confirm_password = StringField('confirm_password',
-        validators=[DataRequired(message='Please confirm your password.')])
+        validators=[DataRequired(message='Please confirm your password.'),
+        Length(min=4, max=255, message='Your passwords must match and be between 4 and 255 characters long.'),])
         # EqualTo('password', message='Passwords must match')])
