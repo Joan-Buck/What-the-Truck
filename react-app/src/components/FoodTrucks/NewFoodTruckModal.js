732ba668e183703ddb5269e2f791cb491c63cadc
Joan-Buck <buck.joan@gmail.com>
2022-03-22 09:16:54 -0600

    Repositioning add new food truck modal button on my food trucks page

--- react-app/src/components/FoodTrucks/NewFoodTruckModal.js
+++ react-app/src/components/FoodTrucks/NewFoodTruckModal.js
@@ -1,24 +1,15 @@
 import React, {useState} from 'react';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import { Modal } from '../../context/Modal';
 import NewFoodTruckForm from './NewFoodTruckForm';
 import './NewFoodTruckForm.css';
 
 function NewFoodTruckModal() {
     const [showModal, setShowModal] = useState(false);
-    const sessionUser = useSelector((state) => state.session.user)
-    const history = useHistory()
-
-    // const modalunauth = (e) => {
-    //     e.stopPropagation()
-    //     history.push('/unauthenticatedUser')
-    // }
 
     return (
-        <div className='new-food-truck-modal-component'>
+        <div className='food-truck-modal-container'>
             {/* TO DO: add plus icon or style at button again - css in NewFoodTruckForm.css */}
-            <button className='new-food-truck-modal-btn' onClick={() => setShowModal(true)}>+ Add new food truck</button>
+            <button className='food-truck-modal-button' onClick={() => setShowModal(true)}>+ Add new food truck</button>
             {showModal && (
                 <Modal onClose={() => setShowModal(false)} >
                     <NewFoodTruckForm className='Modal' closeModal={() => setShowModal(false)}/>
