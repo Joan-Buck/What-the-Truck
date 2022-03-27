5a5877b480378fdc2acaa70dfbaaba0f075d47df
Joan-Buck <buck.joan@gmail.com>
2022-03-27 11:21:58 -0600

    Initial positioning for search results

--- react-app/src/components/Search/Search.js
+++ react-app/src/components/Search/Search.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink, useParams } from 'react-router-dom';
 import FoodTruckCard from '../FoodTrucks/FoodTruckCard';
-import { searchFoodTrucksThunk } from '../../store/foodTrucks';
 import './Search.css'
 
 const Search = () => {
     const params = useParams();
     const paramsSearchItem = params.searchItem;
-    // const dispatch = useDispatch();
     const foodTrucksObj = useSelector(state => state.foodTrucks.entities);
+
     // sorting trucks by category matching
     // const foodTrucks = Object.values(foodTrucksObj).filter(foodTruck => {
     //     return (
@@ -30,66 +29,49 @@ const Search = () => {
     const foodTruckCuisines = Object.values(foodTrucksObj).filter(foodTruck =>
         foodTruck.cuisine.toLowerCase().includes(paramsSearchItem.toLowerCase()))
 
-    // useEffect(() => {
-    //     dispatch(searchFoodTrucksThunk(paramsSearchItem))
-    // }, [dispatch, paramsSearchItem])
-
 
     return (
         <div className={'search-results-container'}>
-            <div className={'search-results-name'}>
-                {/* TO DO: add ternary, if food truck names result show component, else show No Food Trucks match search */}
+            <div className={'search-results-category'}>
                 {foodTruckNames.length > 0 ?
                     <>
-                        <div>Food Trucks with name matching {paramsSearchItem}</div>
+                        <div className={'search-results-title'}>Food Trucks with name matching {paramsSearchItem}</div>
                         <NameResults foodTruckNames={foodTruckNames} />
                     </>
                     :
-                    <div>No Food Trucks were found with a name matching {paramsSearchItem} </div>
+                    <div className={'search-results-none'}>No Food Trucks were found with a name matching {paramsSearchItem} </div>
                 }
-                {/* TO DO: add ternary, if food truck cities result show component, else show No Food Trucks match search */}
+            </div>
+            <div className={'search-results-category'}>
                 {foodTruckCities.length > 0 ?
                     <>
-                        <div>Food Truck with city matching {paramsSearchItem}</div>
+                        <div className={'search-results-title'}>Food Truck with city matching {paramsSearchItem}</div>
                         <CityResults foodTruckCities={foodTruckCities} />
                     </>
                     :
-                    <div>No Food Trucks were found with city matching {paramsSearchItem}</div>
+                    <div className={'search-results-none'}>No Food Trucks were found with city matching {paramsSearchItem}</div>
 
                 }
-                {/* TO DO: add ternary, if food truck cuisine result show component, else show No Food Trucks match search */}
+            </div>
+            <div className={'search-results-category'}>
                 {foodTruckCuisines.length > 0 ?
                     <>
-                        <div>Food Trucks with cuisine matching {paramsSearchItem}</div>
+                        <div className={'search-results-title'}>Food Trucks with cuisine matching {paramsSearchItem}</div>
                         <CuisineResults foodTruckCuisines={foodTruckCuisines} />
                     </>
                     :
-                    <div>No Food Trucks with cuisine matching {paramsSearchItem}</div>
+                    <div className={'search-results-none'}>No Food Trucks with cuisine matching {paramsSearchItem}</div>
                 }
-
-                {/* {foodTrucks.map((foodTruck, i) => (
-                    <div className='food-trucks-card-container' key={i}>
-                        <NavLink className={'food-trucks-card-link'} to={`/food-trucks/${foodTruck.id}`}>
-                            <FoodTruckCard key={foodTruck.id} foodTruck={foodTruck} />
-                        </NavLink>
-                    </div>
-                ))} */}
             </div>
         </div>
     )
 }
 
 
-// TO DO: add component for each type of results
 const NameResults = ({ foodTruckNames }) => {
-    // const foodTruckNames = Object.values(foodTrucksObj).filter(foodTruck =>
-    //     foodTruck.name.toLowerCase().includes(paramsSearchItem.toLowerCase()))
 
     return (
         <>
-            {/* <div className={'search-result-category'}>
-                Food Trucks By Name
-            </div> */}
             {foodTruckNames.map((foodTruck, i) => (
                 <div className='food-trucks-card-container' key={i}>
                     <NavLink className={'food-trucks-card-link'} to={`/food-trucks/${foodTruck.id}`}>
@@ -106,9 +88,6 @@ const CityResults = ({ foodTruckCities }) => {
 
     return (
         <>
-            {/* <div className={'search-result-category'}>
-                Food Trucks By City
-            </div> */}
             {foodTruckCities.map((foodTruck, i) => (
                 <div className='food-trucks-card-container' key={i}>
                     <NavLink className={'food-trucks-card-link'} to={`/food-trucks/${foodTruck.id}`}>
