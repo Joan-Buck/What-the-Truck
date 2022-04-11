460b931193143a355eaa9c0f73e1ebbe1c322d0f
Joan-Buck <buck.joan@gmail.com>
2022-04-11 16:01:10 -0600

    Adding map routes based on walk through

--- app/api/map_routes.py
+++ app/api/map_routes.py
@@ -1,4 +1,10 @@
 from flask import Blueprint
 from flask_googlemaps import GoogleMaps
+from app.config import Config
 
 map_routes = Blueprint('maps', __name__)
+
+
+@map_routes.route('/key', methods=["POST"])
+def get_key():
+    return {"key": Config.FLASK_GOOGLEMAPS_KEY}
