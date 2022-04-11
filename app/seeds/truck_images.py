80d5b9611c91a12b794c807fe85e127df63b9267
Joan-Buck <buck.joan@gmail.com>
2022-04-11 10:51:00 -0600

    Removing two trucks

--- app/seeds/truck_images.py
+++ app/seeds/truck_images.py
@@ -11,22 +11,20 @@ def seed_truck_images():
     image5 = TruckImage(truck_id=5, image_url="https://cdn.vox-cdn.com/thumbor/EuL4Rj61vU6rLve2jzejG9wpb50=/0x0:2000x1334/1820x1213/filters:focal(840x507:1160x827):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/59487203/Pratt_Wood_Shop_BBQ_20.0.7.jpg", created_at=datetime.now(), updated_at=datetime.now())
 
     image6 = TruckImage(truck_id=6, image_url="https://d3hbe0kmbam4a5.cloudfront.net/photos/4bcf993b-14b5-4a04-ba41-300058257425.jpg", created_at=datetime.now(), updated_at=datetime.now())
-    # image7 = TruckImage(truck_id=7, image_url="", created_at=datetime.now(), updated_at=datetime.now())
-    image8 = TruckImage(truck_id=8, image_url="https://assets3.thrillist.com/v1/image/3062949/1584x1054/crop;webp=auto;jpeg_quality=60.jpg", created_at=datetime.now(), updated_at=datetime.now())
-    image9 = TruckImage(truck_id=9, image_url="https://assets3.thrillist.com/v1/image/3062961/1584x1054/crop;webp=auto;jpeg_quality=60.jpg", created_at=datetime.now(), updated_at=datetime.now())
-    image10 = TruckImage(truck_id=10, image_url="https://s3-media0.fl.yelpcdn.com/bphoto/yjf4x2iRNHlA5SDrjLUMDA/o.jpg", created_at=datetime.now(), updated_at=datetime.now())
+    image7 = TruckImage(truck_id=7, image_url="https://assets3.thrillist.com/v1/image/3062949/1584x1054/crop;webp=auto;jpeg_quality=60.jpg", created_at=datetime.now(), updated_at=datetime.now())
+    image8 = TruckImage(truck_id=8, image_url="https://assets3.thrillist.com/v1/image/3062961/1584x1054/crop;webp=auto;jpeg_quality=60.jpg", created_at=datetime.now(), updated_at=datetime.now())
+    image9 = TruckImage(truck_id=9, image_url="https://s3-media0.fl.yelpcdn.com/bphoto/yjf4x2iRNHlA5SDrjLUMDA/o.jpg", created_at=datetime.now(), updated_at=datetime.now())
 
-    image11 = TruckImage(truck_id=11, image_url="https://indianfoodny.com/img/delicious/GrandBazaar.png", created_at=datetime.now(), updated_at=datetime.now())
-    image12 = TruckImage(truck_id=12, image_url="https://infatuation.imgix.net/media/images/reviews/king-souvlaki-of-astoria/banners/1606243773.6297815.png", created_at=datetime.now(), updated_at=datetime.now())
-    # image13 = TruckImage(truck_id=13, image_url="", created_at=datetime.now(), updated_at=datetime.now())
-    image14 = TruckImage(truck_id=14, image_url="https://nyfta.org/wp-content/uploads/2020/09/poke-motion-catering.png", created_at=datetime.now(), updated_at=datetime.now())
-    image15 = TruckImage(truck_id=15, image_url="https://madeinpgh.com/wp-content/uploads/2021/04/image.jpeg", created_at=datetime.now(), updated_at=datetime.now())
+    image10 = TruckImage(truck_id=10, image_url="https://indianfoodny.com/img/delicious/GrandBazaar.png", created_at=datetime.now(), updated_at=datetime.now())
+    image11 = TruckImage(truck_id=11, image_url="https://infatuation.imgix.net/media/images/reviews/king-souvlaki-of-astoria/banners/1606243773.6297815.png", created_at=datetime.now(), updated_at=datetime.now())
+    image12 = TruckImage(truck_id=12, image_url="https://nyfta.org/wp-content/uploads/2020/09/poke-motion-catering.png", created_at=datetime.now(), updated_at=datetime.now())
+    image13 = TruckImage(truck_id=13, image_url="https://madeinpgh.com/wp-content/uploads/2021/04/image.jpeg", created_at=datetime.now(), updated_at=datetime.now())
 
-    image16 = TruckImage(truck_id=16, image_url="https://static.wixstatic.com/media/c7e792_c024361b012c4b73a66ebdb52be2d224~mv2.png/v1/fill/w_1604,h_1040,al_c,usm_0.66_1.00_0.01,enc_auto/IMG_1456_edited_edited.png", created_at=datetime.now(), updated_at=datetime.now())
-    image17 = TruckImage(truck_id=17, image_url="https://d3hbe0kmbam4a5.cloudfront.net/photos/ca89150d-49d0-4e26-89b6-d73513283253.jpg", created_at=datetime.now(), updated_at=datetime.now())
-    image18 = TruckImage(truck_id=18, image_url="https://d3hbe0kmbam4a5.cloudfront.net/photos/83ef42b4-4101-4533-8954-aaf87dfad3c6.jpg", created_at=datetime.now(), updated_at=datetime.now())
-    image19 = TruckImage(truck_id=19, image_url="https://d3hbe0kmbam4a5.cloudfront.net/photos/208cdeb8-23dd-49e9-a24c-a89f6a908cb4.jpeg", created_at=datetime.now(), updated_at=datetime.now())
-    image20 = TruckImage(truck_id=20, image_url="https://d3hbe0kmbam4a5.cloudfront.net/photos/717c0b4c-70a2-49bb-8334-52e4f3857ab2.jpg", created_at=datetime.now(), updated_at=datetime.now())
+    image14 = TruckImage(truck_id=14, image_url="https://static.wixstatic.com/media/c7e792_c024361b012c4b73a66ebdb52be2d224~mv2.png/v1/fill/w_1604,h_1040,al_c,usm_0.66_1.00_0.01,enc_auto/IMG_1456_edited_edited.png", created_at=datetime.now(), updated_at=datetime.now())
+    image15 = TruckImage(truck_id=15, image_url="https://d3hbe0kmbam4a5.cloudfront.net/photos/ca89150d-49d0-4e26-89b6-d73513283253.jpg", created_at=datetime.now(), updated_at=datetime.now())
+    image16 = TruckImage(truck_id=16, image_url="https://d3hbe0kmbam4a5.cloudfront.net/photos/83ef42b4-4101-4533-8954-aaf87dfad3c6.jpg", created_at=datetime.now(), updated_at=datetime.now())
+    image17 = TruckImage(truck_id=17, image_url="https://d3hbe0kmbam4a5.cloudfront.net/photos/208cdeb8-23dd-49e9-a24c-a89f6a908cb4.jpeg", created_at=datetime.now(), updated_at=datetime.now())
+    image18 = TruckImage(truck_id=18, image_url="https://d3hbe0kmbam4a5.cloudfront.net/photos/717c0b4c-70a2-49bb-8334-52e4f3857ab2.jpg", created_at=datetime.now(), updated_at=datetime.now())
 
     db.session.add(image1)
     db.session.add(image2)
@@ -46,8 +44,7 @@ def seed_truck_images():
     db.session.add(image16)
     db.session.add(image17)
     db.session.add(image18)
-    db.session.add(image19)
-    db.session.add(image20)
+
 
     db.session.commit()
 
--- app/seeds/trucks.py
+++ app/seeds/trucks.py
@@ -1,8 +1,6 @@
 from app.models import db, Truck
 from datetime import datetime
 
-# lat: 40.17012,
-#   lng: -105.13876
 
 def seed_trucks():
     truck1 = Truck(owner_id=1, name="Taco Love", address="4637 Market St", city="San Diego", state="California", zip_code="92102", cuisine="Tacos/Burritos", price="$$", lat=31.7110774, long=-117.0949059, created_at=datetime.now(), updated_at=datetime.now())
@@ -12,22 +10,20 @@ def seed_trucks():
     truck5 = Truck(owner_id=1, name="Wood Shop BBQ", address="2513 S Jackson St", city="Seattle", state="Washington", zip_code="98144", cuisine="BBQ", price="$$", lat=47.599060, long=-122.299140, created_at=datetime.now(), updated_at=datetime.now())
 
     truck6 = Truck(owner_id=2, name="Yeti Dogs", address="66 Bell Street", city="Seattle", state="Washington", zip_code="98121", cuisine="Sandwiches", price="$", lat=47.612690, long=-122.347900, created_at=datetime.now(), updated_at=datetime.now())
-    # truck7 = Truck(owner_id=2, name="Uncle John's BBQ", address="4417 Seahawks Way", city="Seattle", state="Washington", zip_code="98126", cuisine="BBQ", price="$$", lat=None, long=None, created_at=datetime.now(), updated_at=datetime.now())
-    truck8 = Truck(owner_id=2, name="Cold Cookie Co.", address="2512 Rio Grande St", city="Austin", state="Texas", zip_code="78705", cuisine="Ice Cream", price="$", lat=30.290970729236854, long=-97.74457029563469, created_at=datetime.now(), updated_at=datetime.now())
-    truck9 = Truck(owner_id=2, name="Hey Cupcake!", address="5700 Menchaca Rd", city="Austin", state="Texas", zip_code="78745", cuisine="Dessert", price="$$", lat=30.21599076497917, long=-97.80012936497467, created_at=datetime.now(), updated_at=datetime.now())
-    truck10 = Truck(owner_id=2, name="Dawa Sushi", address="5000 Burnet Rd", city="Austin", state="Texas", zip_code="78756", cuisine="Sushi", price="$$$", lat=30.321879559398727, long=-97.7384649580673, created_at=datetime.now(), updated_at=datetime.now())
+    truck7 = Truck(owner_id=2, name="Cold Cookie Co.", address="2512 Rio Grande St", city="Austin", state="Texas", zip_code="78705", cuisine="Ice Cream", price="$", lat=30.290970729236854, long=-97.74457029563469, created_at=datetime.now(), updated_at=datetime.now())
+    truck8 = Truck(owner_id=2, name="Hey Cupcake!", address="5700 Menchaca Rd", city="Austin", state="Texas", zip_code="78745", cuisine="Dessert", price="$$", lat=30.21599076497917, long=-97.80012936497467, created_at=datetime.now(), updated_at=datetime.now())
+    truck9 = Truck(owner_id=2, name="Dawa Sushi", address="5000 Burnet Rd", city="Austin", state="Texas", zip_code="78756", cuisine="Sushi", price="$$$", lat=30.321879559398727, long=-97.7384649580673, created_at=datetime.now(), updated_at=datetime.now())
+    truck10 = Truck(owner_id=3, name="Mysttik Masaala", address="399 Park Ave", city="New York", state="New York", zip_code="10022", cuisine="Indian", price="$$$", lat=40.760061736945886, long=-73.96941901663354, created_at=datetime.now(), updated_at=datetime.now())
 
-    truck11 = Truck(owner_id=3, name="Mysttik Masaala", address="399 Park Ave", city="New York", state="New York", zip_code="10022", cuisine="Indian", price="$$$", lat=40.760061736945886, long=-73.96941901663354, created_at=datetime.now(), updated_at=datetime.now())
-    truck12 = Truck(owner_id=3, name="King Souvlaki of Astoria", address="30 31st St", city="New York", state="New York", zip_code="11106", cuisine="Sandwiches", price="$", lat=40.765090937735415, long=-73.9235053651495, created_at=datetime.now(), updated_at=datetime.now())
-    # truck13 = Truck(owner_id=3, name="Patacon Pisao", address="139 Essex St", city="New York", state="New York", zip_code="10002", cuisine="Sandwiches", price="$$", lat=None, long=None, created_at=datetime.now(), updated_at=datetime.now())
-    truck14 = Truck(owner_id=3, name="Poke Motion", address="1152 Broadway Ave", city="New York", state="New York", zip_code="10022", cuisine="Sushi", price="$$$$", lat=40.74514709871435, long=-73.98872765259263, created_at=datetime.now(), updated_at=datetime.now())
-    truck15 = Truck(owner_id=3, name="Boston Coffee Rescue", address="8127 Main St", city="Boston", state="Massachusetts", zip_code="02199", cuisine="Coffee", price="$", lat=42.37953528980907, long=-71.06715916873426, created_at=datetime.now(), updated_at=datetime.now())
+    truck11 = Truck(owner_id=3, name="King Souvlaki of Astoria", address="30 31st St", city="New York", state="New York", zip_code="11106", cuisine="Sandwiches", price="$", lat=40.765090937735415, long=-73.9235053651495, created_at=datetime.now(), updated_at=datetime.now())
+    truck12 = Truck(owner_id=3, name="Poke Motion", address="1152 Broadway Ave", city="New York", state="New York", zip_code="10022", cuisine="Sushi", price="$$$$", lat=40.74514709871435, long=-73.98872765259263, created_at=datetime.now(), updated_at=datetime.now())
+    truck13 = Truck(owner_id=3, name="Boston Coffee Rescue", address="8127 Main St", city="Boston", state="Massachusetts", zip_code="02199", cuisine="Coffee", price="$", lat=42.37953528980907, long=-71.06715916873426, created_at=datetime.now(), updated_at=datetime.now())
+    truck14 = Truck(owner_id=4, name="Taco Calleteco", address="1 City Hall Square", city="Boston", state="Massachusetts", zip_code="02201", cuisine="Tacos/Burritos", price="$$$", lat=42.360813953788856, long= -71.05681373752188, created_at=datetime.now(), updated_at=datetime.now())
+    truck15 = Truck(owner_id=4, name="Trolley Dogs", address="1 City Hall Square", city="Boston", state="Massachusetts", zip_code="02201", cuisine="Sandwiches", price="$$", lat=42.360813953788856, long= -71.05681373752188, created_at=datetime.now(), updated_at=datetime.now())
 
-    truck16 = Truck(owner_id=4, name="Taco Calleteco", address="1 City Hall Square", city="Boston", state="Massachusetts", zip_code="02201", cuisine="Tacos/Burritos", price="$$$", lat=42.360813953788856, long= -71.05681373752188, created_at=datetime.now(), updated_at=datetime.now())
-    truck17 = Truck(owner_id=4, name="Trolley Dogs", address="1 City Hall Square", city="Boston", state="Massachusetts", zip_code="02201", cuisine="Sandwiches", price="$$", lat=42.360813953788856, long= -71.05681373752188, created_at=datetime.now(), updated_at=datetime.now())
-    truck18 = Truck(owner_id=4, name="Em's Ice Cream", address="2829 Fairfax St", city="Denver", state="Colorado", zip_code="80207", cuisine="Ice Cream", price="$$", lat=39.75769314439567, long=-104.92701291604337, created_at=datetime.now(), updated_at=datetime.now())
-    truck19 = Truck(owner_id=4, name="Peaceful Creations", address="5160 Main St", city="Denver", state="Colorado", zip_code="80207", cuisine="Ice Cream", price="$", lat=39.78216242843816, long=-104.89208539908391, created_at=datetime.now(), updated_at=datetime.now())
-    truck20 = Truck(owner_id=4, name="Barefoot BBQ", address="701 12th St", city="Golden", state="Colorado", zip_code="80401", cuisine="BBQ", price="$$", lat=39.75635804630244, long=-105.21923913879182, created_at=datetime.now(), updated_at=datetime.now())
+    truck16 = Truck(owner_id=4, name="Em's Ice Cream", address="2829 Fairfax St", city="Denver", state="Colorado", zip_code="80207", cuisine="Ice Cream", price="$$", lat=39.75769314439567, long=-104.92701291604337, created_at=datetime.now(), updated_at=datetime.now())
+    truck17 = Truck(owner_id=4, name="Peaceful Creations", address="5160 Main St", city="Denver", state="Colorado", zip_code="80207", cuisine="Ice Cream", price="$", lat=39.78216242843816, long=-104.89208539908391, created_at=datetime.now(), updated_at=datetime.now())
+    truck18 = Truck(owner_id=4, name="Barefoot BBQ", address="701 12th St", city="Golden", state="Colorado", zip_code="80401", cuisine="BBQ", price="$$", lat=39.75635804630244, long=-105.21923913879182, created_at=datetime.now(), updated_at=datetime.now())
 
     db.session.add(truck1)
     db.session.add(truck2)
@@ -47,8 +43,6 @@ def seed_trucks():
     db.session.add(truck16)
     db.session.add(truck17)
     db.session.add(truck18)
-    db.session.add(truck19)
-    db.session.add(truck20)
 
     db.session.commit()
 
